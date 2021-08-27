18e125eb40d4991ae24e3f659c50a054b5686ead
Victor Andevert <victor.andevert@gmail.com>
2021-08-27 06:53:08 +0200

    refactor grid forward and backward methods

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -5,59 +5,38 @@ import arrow.core.Some
 
 class Grid(private val obstacles: List<Pair<Int,Int>> = emptyList()) {
 
-    fun move(direction: Char, position: Pair<Int,Int>, orientation: Orientation): Option<Pair<Int, Int>> {
-        return when (direction) {
+    fun move(direction: Char, position: Pair<Int,Int>, orientation: Orientation) =
+        when (direction) {
             'F' -> moveForwardTo(position, orientation)
             else -> moveBackwardTo(position, orientation)
         }
-    }
 
     private fun moveForwardTo(position: Pair<Int,Int>, orientation: Orientation): Option<Pair<Int,Int>>{
         var (x, y) = position
-
-        if (orientation.isNorth())
-            y = increase(y)
-
-        if(orientation.isEast())
-            x = increase(x)
-
-        if (orientation.isWest())
-            x =  decrease(x)
-
-        if(orientation.isSouth())
-            y = decrease(y)
-
-        val p = Pair(x,y)
-
-        return when {
-            obstacles.contains(p) -> None
-            else -> Some(p)
+        when {
+            orientation.isNorth() -> increase(y).also { y = it }
+            orientation.isSouth() -> decrease(y).also { y = it }
+            orientation.isEast() -> increase(x).also { x = it }
+            orientation.isWest() -> decrease(x).also { x = it }
         }
+        return findObstacles(x to y)
     }
 
 
     private fun moveBackwardTo(position: Pair<Int,Int>, orientation: Orientation): Option<Pair<Int,Int>>{
         var (x, y) = position
-
-        if (orientation.isNorth())
-            y = decrease(y)
-
-        if(orientation.isEast())
-            x = decrease(x)
-
-        if(orientation.isWest())
-            x = increase(x)
-
-        if(orientation.isSouth())
-            y = increase(y)
-
-        val p = Pair(x,y)
-
-        return when {
-            obstacles.contains(p) -> None
-            else -> Some(p)
+        when {
+            orientation.isNorth() -> decrease(y).also { y = it }
+            orientation.isSouth() -> increase(y).also { y = it }
+            orientation.isEast() -> decrease(x).also { x = it }
+            orientation.isWest() -> increase(x).also { x = it }
         }
+        return findObstacles(x to y)
+    }
 
+    private fun findObstacles(position: Pair<Int,Int>) = when (position) {
+        in obstacles -> None
+        else -> Some(position)
     }
 
     private val maxSquares = 10
